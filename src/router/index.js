5ba741c02ef1196421d80fc9574c350d7f072082
lucf <2572274368@qq.com>
2023-11-28 15:46:58 +0800

    代码

---
+++ src/router/index.js
@@ -0,0 +1,120 @@
+// 导入组件
+import Vue from 'vue';
+import Router from 'vue-router';
+// 登录
+import login from '@/views/login';
+// 首页
+import index from '@/views/index';
+/**
+ * 基础菜单
+ */
+// 商品管理
+import Goods from '@/views/goods/Goods';
+/**
+ * 系统管理
+ */
+// 用户管理
+import user from '@/views/system/user';
+// 菜单管理
+import Module from '@/views/system/Module';
+// 角色管理
+import Role from '@/views/system/Role';
+// 系统环境变量
+import Variable from '@/views/system/Variable';
+// 权限管理
+import Permission from '@/views/system/Permission';
+
+/**
+ * 数据监控
+ */
+// 监控查询
+import druidLogin from '@/views/druid/login';
+
+// 图表界面
+import statistics from '@/views/charts/statistics';
+
+// 启用路由
+Vue.use(Router);
+
+// 导出路由
+export default new Router({
+    routes: [{
+        path: '/',
+        name: '',
+        component: login,
+        hidden: true,
+        meta: {
+            requireAuth: false
+        }
+    }, {
+        path: '/login',
+        name: '登录',
+        component: login,
+        hidden: true,
+        meta: {
+            requireAuth: false
+        }
+    }, {
+        path: '/index',
+        name: '首页',
+        component: index,
+        iconCls: 'el-icon-tickets',
+        children: [{
+            path: '/goods/Goods',
+            name: '商品管理',
+            component: Goods,
+            meta: {
+                requireAuth: true
+            }
+        },{
+            path: '/system/user',
+            name: '用户管理',
+            component: user,
+            meta: {
+                requireAuth: true
+            }
+        }, {
+            path: '/system/Module',
+            name: '菜单管理',
+            component: Module,
+            meta: {
+                requireAuth: true
+            }
+        }, {
+            path: '/system/Role',
+            name: '角色管理',
+            component: Role,
+            meta: {
+                requireAuth: true
+            }
+        }, {
+            path: '/system/Variable',
+            name: '系统环境变量',
+            component: Variable,
+            meta: {
+                requireAuth: true
+            }
+        }, {
+            path: '/system/Permission',
+            name: '权限管理',
+            component: Permission,
+            meta: {
+                requireAuth: true
+            }
+        },{
+            path: '/druid/login',
+            name: '监控查询',
+            component: druidLogin,
+            meta: {
+                requireAuth: true
+            }
+        }, {
+            path: '/charts/statistics',
+            name: '数据可视化',
+            component: statistics,
+            meta: {
+                requireAuth: true
+            }
+        }]
+    }]
+})
