7ba2179638b459a2b5b3d5a4cea47bf9b1efde63
lucf <2572274368@qq.com>
2023-12-08 16:59:35 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-/*
- * @Author: “chenjinwei” “507725948@qq.com”
- * @Date: 2023-10-26 11:49:01
- * @LastEditors: lucf 2572274368@qq.com
- * @LastEditTime: 2023-12-07 15:22:44
- * @FilePath: \vue2-template\src\main.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import Vue from 'vue'
 import App from './App.vue'
 import 'bytemd/dist/index.css'
-
 
 import router from '@/router'
 import store from '@/store'
@@ -18,6 +9,8 @@ import '@/utils/auto_import_elementUI';
 import 'element-ui/lib/theme-chalk/index.css';
 // 引入icon
 import './assets/icon/iconfont.css'
+//引入公共样式
+import './assets/styles/rest.scss'
 
 import "@/directives/dialogDrag"
 // 过滤器
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,5 @@
+import NProgress from "nprogress"; // 导入 nprogress
+import "../assets/styles/nprogress.scss"; // 导入样式，否则看不到效果
 // 导入组件
 import Vue from "vue";
 import Router from "vue-router";
@@ -27,30 +29,28 @@ import Module from "@/views/system/Module";
 // // 图表界面
 import statistics from "@/views/charts/statistics";
 
-
 // 启用路由
 Vue.use(Router);
 
 // 导出路由
-export default new Router({
-  routes: [
+export const constantRoutes = [
     {
-      path: "/",
-      name: "",
-      component: login,
-      hidden: true,
-      meta: {
-        requireAuth: false
-      }
+        path: "/",
+        name: "",
+        component: login,
+        hidden: true,
+        meta: {
+            requireAuth: false,
+        },
     },
     {
-      path: "/login",
-      name: "登录",
-      component: login,
-      hidden: true,
-      meta: {
-        requireAuth: false
-      }
+        path: "/login",
+        name: "登录",
+        component: login,
+        hidden: true,
+        meta: {
+            requireAuth: false,
+        },
     },
     {
         path: "/editor",
@@ -58,65 +58,80 @@ export default new Router({
         component: editor,
         hidden: true,
         meta: {
-          requireAuth: false
-        }
-      },
-    {
-      path: "/index",
-      name: "首页",
-      component: index,
-      iconCls: "el-icon-tickets",
-      children: [
-        {
-          path: "/goods/Goods",
-          name: "文章列表",
-          component: Goods,
-          meta: {
-            requireAuth: true
-          }
+            requireAuth: false,
         },
-        {
-            path: "/project/index",
-            name: "项目列表",
-            component: Project,
-            meta: {
-              requireAuth: true
-            }
-          },
-          {
-            path: "/message/index",
-            name: "留言列表",
-            component: Message,
-            meta: {
-              requireAuth: true
-            }
-          },
-          {
-            path: "/system/user",
-            name: "用户管理",
-            component: user,
-            meta: {
-              requireAuth: true
-            }
-          },
-          {
-            path: "/system/Module",
-            name: "菜单管理",
-            component: Module,
-            meta: {
-              requireAuth: true
-            }
-          },
-          {
-            path: "/charts/statistics",
-            name: "数据可视化",
-            component: statistics,
-            meta: {
-              requireAuth: true
-            }
-          },
-      ]
-    }
-  ]
+    },
+    {
+        path: "/index",
+        name: "首页",
+        component: index,
+        iconCls: "el-icon-tickets",
+        children: [
+            {
+                path: "/goods/Goods",
+                name: "文章列表",
+                component: Goods,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+            {
+                path: "/project/index",
+                name: "项目列表",
+                component: Project,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+            {
+                path: "/message/index",
+                name: "留言列表",
+                component: Message,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+            {
+                path: "/system/user",
+                name: "用户管理",
+                component: user,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+            {
+                path: "/system/Module",
+                name: "菜单管理",
+                component: Module,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+            {
+                path: "/charts/statistics",
+                name: "数据可视化",
+                component: statistics,
+                meta: {
+                    requireAuth: true,
+                },
+            },
+        ],
+    },
+];
+NProgress.configure({ showSpinner: true }); // 显示右上角螺旋加载提示
+const createRouter = () =>
+    new Router({
+        scrollBehavior: () => ({ y: 0 }),
+        routes: constantRoutes,
+    });
+const router = createRouter();
+router.beforeEach((to, from, next) => {
+    NProgress.start(); // 开启进度条
+    next();
+    NProgress.done(); // 关闭进度条
 });
-
+export function resetRouter() {
+    const newRouter = createRouter();
+    router.matcher = newRouter.matcher; // reset router
+}
+export default router;
